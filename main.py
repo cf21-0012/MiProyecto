4f353c8fd813514eebf9f52d20168640ff925fbe
Tino <cf21-0012@unphu.edu.do>
2025-01-25 00:00:10 -0400

    Create Main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+#Programa que imprime tu nombre por teclado.
+# Nombre
+nombre = input("Por favor, ingresa tu nombre: ")
+
+# Apellido
+apellido = input("Ahora, ingresa tu apellido: ")
+
+# Print en consola.
+print(f"¡Hola soy, {nombre} {apellido}! Espero que tengas un buen dia!.")
